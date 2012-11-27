ef9c82cb732291dc6c0ffd9b7b88b412c38091da
czarek <czarek.zawadka@gmail.com>
2012-11-27 20:57:14 +0100

    Na dobry początek: klasa z miarą Manhatan

---
+++ Clust/src/clust/ManhatanDistance.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clust;
+
+/**
+ *
+ * @author czarek
+ */
+public class ManhatanDistance implements Distance{
+    
+    @Override
+    public double compute(Color a, Color b) {
+        double ret = 0.0;
+        for(int i=0;i<a.vec.length;i++){
+            ret += Math.abs(a.vec[i]-b.vec[i]);
+        }
+        return ret;
+    }
+}
