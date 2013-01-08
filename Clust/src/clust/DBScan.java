15336e05a9acea57743ee852f5c7334d503a11f9
gacekjr56 <gacekjr56@gmail.com>
2013-01-08 19:24:06 +0100

    DBScan changes

--- Clust/src/clust/DBScan.java
+++ Clust/src/clust/DBScan.java
@@ -3,148 +3,204 @@ package clust;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.List;
 import javax.imageio.ImageIO;
+import java.io.*;
 
 /**
  *
  * @author Marcin
  */
 public class DBScan {
-
-	Distance dist;
-
-	public DBScan(Distance dist){
-		this.dist = dist;
-	}
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) throws IOException {
-		DBScan dbs = new DBScan(new EuclideanDistance());
-//		DBScan dbs = new DBScan(new ManhatanDistance());
-		String fileIn = "images/ref1.jpg";
-		String fileOut = "images/ref1_out2";
-		BufferedImage image = ImageIO.read(new File(fileIn));
-
-		double eps = 5;
-		int minPts = 10;
-		double eps1 = 2.500, eps2 = 2.500, epsSkip = 0.01000000000000000;
-		int MinPts1 = 2, MinPts2 = 2, MinPtsSkip = 1, epsDiv=100;
-		int best[][] = new int[5][2];
-
-		int pix,r,g,b,k=0;
-		Color[] colors = new Color[image.getHeight()*image.getWidth()];
-
-		//wczytanie danych o kolorach
-		for(int i=0;i<image.getWidth();i++){
-			for(int j=0;j<image.getHeight();j++){
-				pix = image.getRGB(i, j);
-				r = (pix >> 16) & 0xFF;
-				g = (pix >> 8) & 0xFF;
-				b = pix & 0xFF;
-				colors[k++] = new Color(r,g,b);
-			}
-		}
-
-		for(minPts=MinPts1;minPts<=MinPts2;minPts+=MinPtsSkip)
-		{
-			for(eps=eps1;eps<=eps2;eps+=epsSkip)
-			{
-				DBScanClusters clust = new DBScanClusters(colors, dbs.dist);
-				int nClust = 3, imgSize = image.getHeight()*image.getWidth();
-				clust.DBSCAN(eps,minPts);
-				nClust=clust.nClust;
-				System.out.println("liczba klastrów " + nClust);
-
-				Color reps[] = new Color[nClust];
-				k=0;
-				for(int i=1;i<= nClust;i++){
-					if(!clust.clusts[i].isEmpty()){
-						reps[k++] = dbs.findRep(clust.clusts[i]);
-						int tmp = clust.clusts[i].size();
-						System.out.println("wielkosc klastra: " + clust.clusts[i].size());
-						int tmp2=0;
-						if (tmp > best[4][0])
-						{
-							best[0]=best[1].clone();
-							best[1]=best[2].clone();
-							best[2]=best[3].clone();
-							best[3]=best[4].clone();
-							best[4]=new int[]{tmp,i};
-						}else if (tmp > best[3][0])
-						{
-							best[0]=best[1].clone();
-							best[1]=best[2].clone();
-							best[2]=best[3].clone();
-							best[3]=new int[]{tmp,i};
-						}else if (tmp > best[2][0])
-						{
-							best[0]=best[1].clone();
-							best[1]=best[2].clone();
-							best[2]=new int[]{tmp,i};
-						}else if (tmp > best[1][0])
-						{
-							best[0]=best[1].clone();
-							best[1]=new int[]{tmp,i};
-						}else if (tmp > best[0][0])
-						{
-							best[0]=new int[]{tmp,i};
-						}
-					}
-				}
-				System.out.println("Najliczniejsze: " + best[0][0] + "_"+ best[1][0]+"_"+ best[2][0]+ "_"+ best[3][0]+ "_"+ best[4][0]);
-				
-				
-				imgSize /= k;
-				int rep = 0;
-				for(int i=0;i<image.getWidth();i++){
-					for(int j=0;j<image.getHeight();j++){
-						pix = reps[rep/(imgSize+1)].toInt();//clust.clusts[c].get(0).toInt();
-						image.setRGB(i,j,pix);
-						rep++;
-					}
-				}
-				eps = eps*epsDiv;
-				eps = Math.round(eps);
-				eps = eps /epsDiv;
-				ImageIO.write(image, "jpg", new File(fileOut+"_"+eps+"_"+minPts+"_"+nClust+".jpg"));
-				
-				imgSize =(image.getHeight()*image.getWidth())/5;
-				rep = 0;
-				for(int i=0;i<image.getWidth();i++){
-					for(int j=0;j<image.getHeight();j++){
-						pix = reps[best[rep/(imgSize+1)][1]].toInt();//clust.clusts[c].get(0).toInt();
-						image.setRGB(i,j,pix);
-						rep++;
-					}
-				}
-				eps = eps*epsDiv;
-				eps = Math.round(eps);
-				eps = eps /epsDiv;
-				ImageIO.write(image, "jpg", new File(fileOut+"_best_"+eps+"_"+minPts+"_"+nClust+".jpg"));
-			}
-		}
-	}
-
-	public Color findRep(List<Color> colors){
-		double r=0.0,g=0.0,b=0.0,dst = Double.POSITIVE_INFINITY,tmp;
-		double s = (double)colors.size();
-		Color ret = new Color(0,0,0);
-		for(Color c : colors){
-			r += c.vec[0];
-			g += c.vec[1];
-			b += c.vec[2];
-		}
-		r /= s;
-		g /= s;
-		b /= s;
-		Color crep = new Color((int)r,(int)g,(int)b);
-		for(Color c : colors){
-			tmp = dist.compute(c, crep);
-			if(tmp < dst){dst = tmp; ret = c;}
-		}
-		return ret;
-//		return crep;
-	}
+    
+    Distance dist;
+    
+    public DBScan(Distance dist){
+        this.dist = dist;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        
+        DBScan dbs = new DBScan(new EuclideanDistance());
+        //DBScan dbs = new DBScan(new ManhatanDistance());
+        String fileIn = "images/icon_9.jpg";
+        String fileOut = "images/out_KMEANS/icon_9b/icon_9_out";
+        PrintWriter myout = new PrintWriter(new FileWriter(fileOut+".txt"),true);
+        BufferedImage image = ImageIO.read(new File(fileIn));
+        
+        double eps = 5;
+        int minPts = 10;
+        double eps1 = 0.500, eps2 = 20.000, epsSkip = 0.50000000000000000;
+        int MinPts1 = 1, MinPts2 = 15, MinPtsSkip = 1, epsDiv=100;
+        int best[][] = new int[7][2];
+        
+        int pix,r,g,b,k=0;
+        Color[] colors = new Color[image.getHeight()*image.getWidth()];
+        
+        //wczytanie danych o kolorach
+        for(int i=0;i<image.getWidth();i++){
+            for(int j=0;j<image.getHeight();j++){
+                pix = image.getRGB(i, j);
+                r = (pix >> 16) & 0xFF;
+                g = (pix >> 8) & 0xFF;
+                b = pix & 0xFF;
+                colors[k++] = new Color(r,g,b);
+            }
+        }
+        
+        for(minPts=MinPts1;minPts<=MinPts2;minPts+=MinPtsSkip)
+        {
+            for(eps=eps1;eps<=eps2;eps+=epsSkip)
+            {
+                DBScanClusters clust = new DBScanClusters(colors, dbs.dist);
+                int nClust = 3, imgSize = image.getHeight()*image.getWidth();
+                clust.DBSCAN(eps,minPts);
+                nClust=clust.nClust;
+                System.out.println("minPts= " + minPts + " eps= "+eps);
+                System.out.println("liczba klastrów " + nClust);
+                    
+                Color reps[] = new Color[nClust+1];
+                k=0;
+                for(int i=1;i<= nClust;i++){
+                    if(!clust.clusts[i].isEmpty()){
+                        reps[k++] = dbs.findRep(clust.clusts[i]);
+                        int tmp = clust.clusts[i].size();
+                        //System.out.println("wielkosc klastra: " + clust.clusts[i].size());
+                        boolean check = true;
+                        for(int j=0;j<7;++j)
+                        {
+                            if (tmp > best[j][0] && check)
+                            {
+                                check = false;
+                                for(int l=6;l>j;l--)
+                                {
+                                    best[l]=best[l-1].clone();
+                                }
+                                best[j]=new int[]{tmp,i-1};
+                            }
+                        }
+                    }
+                }
+                myout.write(fileOut+"_"+eps+"_"+minPts+"_"+nClust+".bmp"+"\n");
+                myout.write("eps="+eps+" minPts="+minPts+" nClust="+nClust+"\n");
+                for (int i=0;i<7;i++)
+                {
+                    //myout.write("testing");
+                    myout.write("Liczba: "+best[i][0]+"   \t Kolor: " + reps[best[i][1]]+"\n");
+                }
+                myout.write("\n");
+                if (k==0) 
+                    k=1;
+                
+                imgSize /= k;
+                int rep = 0;
+                for(int i=0;i<image.getWidth();i++){
+                    for(int j=0;j<image.getHeight();j++){
+                        if(nClust==0)
+                            image.setRGB(i,j,1);
+                        else
+                        {
+                            pix = reps[rep/(imgSize+1)].toInt();
+                            image.setRGB(i,j,pix);
+                            rep++;
+                        }
+                    }
+                }
+                eps = eps*epsDiv;
+                eps = Math.round(eps);
+                eps = eps /epsDiv;
+                ImageIO.write(image, "bmp", new File(fileOut+"_"+eps+"_"+minPts+"_"+nClust+".bmp"));
+                
+                // return 7 main colors
+                imgSize =(image.getHeight()*image.getWidth())/7;
+                rep = 0;
+                for(int i=0;i<image.getWidth();i++){
+                    for(int j=0;j<image.getHeight();j++){
+                        if(nClust==0)
+                            image.setRGB(i,j,1);
+                        else
+                        {
+                            pix = reps[best[rep/(imgSize+1)][1]].toInt();
+                            image.setRGB(i,j,pix);
+                            rep++;
+                        }
+                    }
+                }
+                eps = eps*epsDiv;
+                eps = Math.round(eps);
+                eps = eps /epsDiv;
+                ImageIO.write(image, "bmp", new File(fileOut+"_"+eps+"_"+minPts+"_"+nClust+"_best7.bmp"));
+                
+                // return 5 main colors
+                imgSize =(image.getHeight()*image.getWidth())/5;
+                rep = 0;
+                for(int i=0;i<image.getWidth();i++){
+                    for(int j=0;j<image.getHeight();j++){
+                        if(nClust==0)
+                            image.setRGB(i,j,1);
+                        else
+                        {
+                            pix = reps[best[rep/(imgSize+1)][1]].toInt();
+                            image.setRGB(i,j,pix);
+                            rep++;
+                        }
+                    }
+                }
+                eps = eps*epsDiv;
+                eps = Math.round(eps);
+                eps = eps /epsDiv;
+                ImageIO.write(image, "bmp", new File(fileOut+"_"+eps+"_"+minPts+"_"+nClust+"_best5.bmp"));
+                
+                // return 3 main colors
+                imgSize =(image.getHeight()*image.getWidth())/3;
+                rep = 0;
+                for(int i=0;i<image.getWidth();i++){
+                    for(int j=0;j<image.getHeight();j++){
+                        if(nClust==0)
+                            image.setRGB(i,j,1);
+                        else
+                        {
+                            pix = reps[best[rep/(imgSize+1)][1]].toInt();
+                            image.setRGB(i,j,pix);
+                            rep++;
+                        }
+                    }
+                }
+                eps = eps*epsDiv;
+                eps = Math.round(eps);
+                eps = eps /epsDiv;
+                ImageIO.write(image, "bmp", new File(fileOut+"_"+eps+"_"+minPts+"_"+nClust+"_best3.bmp"));
+                
+                for(int j=0;j<7;j++)
+                {
+                    best[j][0]=0;
+                    best[j][1]=0;
+                }
+            }
+        }
+        myout.close();
+    }
+    
+    public Color findRep(List<Color> colors){
+        double r=0.0,g=0.0,b=0.0,dst = Double.POSITIVE_INFINITY,tmp;
+        double s = (double)colors.size();
+        Color ret = new Color(0,0,0);
+        for(Color c : colors){
+            r += c.vec[0];
+            g += c.vec[1];
+            b += c.vec[2];
+        }
+        r /= s;
+        g /= s;
+        b /= s;
+        Color crep = new Color((int)r,(int)g,(int)b);
+        for(Color c : colors){
+            tmp = dist.compute(c, crep);
+            if(tmp < dst){dst = tmp; ret = c;}
+        }
+        return ret;
+    }
 }
--- Clust/src/clust/DBScanClusters.java
+++ Clust/src/clust/DBScanClusters.java
@@ -17,8 +17,8 @@ public class DBScanClusters {
     
     @SuppressWarnings("unchecked")
     public DBScanClusters(Color[] colors, Distance dist){
-    	clusts = (List<Color>[]) new ArrayList[colors.length];
-    	clusts[0] = new ArrayList(); //SZUM
+        clusts = (List<Color>[]) new ArrayList[colors.length];
+        clusts[0] = new ArrayList(); //SZUM
         visited = new boolean[colors.length];
         this.colors = new Color[colors.length];
         System.arraycopy( colors, 0, this.colors, 0, colors.length );
@@ -31,73 +31,72 @@ public class DBScanClusters {
     
     public void DBSCAN (double eps, int MinPts)
     {
-    	for(int i=0;i<colors.length;i++){
-    		if (!visited[i])
-    		{
-    			visited[i]=true;
-    			counter--;
-    			System.out.println("nieodwiedzonych: " + counter + "   NOWY KLASTER");
-    			List<Integer> NeighborPts = (List<Integer>) regionQuery(i, eps);
-    			if (NeighborPts.size() < MinPts){
-    				//mark as NOISE
-    				clusts[0].add(colors[i]);
-    			}else
-    			{
-    				//make new cluster
-    				nClust++;
-    	            clusts[nClust] = new ArrayList();
-    				expandCluster(i,NeighborPts,nClust,eps,MinPts);
-    			}
-    		}
-    	}
+        for(int i=0;i<colors.length;i++){
+            if (!visited[i])
+            {
+                visited[i]=true;
+                counter--;
+                //System.out.println("nieodwiedzonych: " + counter + "   NOWY KLASTER");
+                List<Integer> NeighborPts = (List<Integer>) regionQuery(i, eps);
+                if (NeighborPts.size() < MinPts){
+                    //mark as NOISE
+                    clusts[0].add(colors[i]);
+                }else
+                {
+                    //make new cluster
+                    nClust++;
+                    clusts[nClust] = new ArrayList();
+                    expandCluster(i,NeighborPts,nClust,eps,MinPts);
+                }
+            }
+        }
     }
-
+    
     private void expandCluster (int P, List<Integer> NeighborPts, int nClust, double eps, int MinPts)
     {
         clusts[nClust].add((Color)colors[P]);
         for(int i=0;i<NeighborPts.size();i++){
-        	if (!visited[NeighborPts.get(i)])
-        	{
-        		visited[NeighborPts.get(i)]=true;
-    			counter--;
-    			System.out.println("nieodwiedzonych: " + counter);
-        		//List<Integer> NeighborPtsTmp = (List<Integer>) regionQuery(i, eps);
-    			List<Integer> NeighborPtsTmp = (List<Integer>) regionQuery(NeighborPts.get(i), eps);
-        		if (NeighborPtsTmp.size() >= MinPts){
-        			for(int j=0 ; j< NeighborPtsTmp.size();j++)
-        				if (!NeighborPts.contains(NeighborPtsTmp.get(j)))
-        					NeighborPts.add(NeighborPtsTmp.get(j));
-        			NeighborPtsTmp.clear();
-        		}
-        	}
-        	if (!checkInClusters(NeighborPts.get(i))){
-        		clusts[nClust].add((Color)colors[NeighborPts.get(i)]);
-        	}
-        }	
+            if (!visited[NeighborPts.get(i)])
+            {
+                visited[NeighborPts.get(i)]=true;
+                counter--;
+                //System.out.println("nieodwiedzonych: " + counter);
+                List<Integer> NeighborPtsTmp = (List<Integer>) regionQuery(NeighborPts.get(i), eps);
+                if (NeighborPtsTmp.size() >= MinPts){
+                    for(int j=0 ; j< NeighborPtsTmp.size();j++)
+                        if (!NeighborPts.contains(NeighborPtsTmp.get(j)))
+                            NeighborPts.add(NeighborPtsTmp.get(j));
+                    NeighborPtsTmp.clear();
+                }
+            }
+            if (!checkInClusters(NeighborPts.get(i))){
+                clusts[nClust].add((Color)colors[NeighborPts.get(i)]);
+            }
+        }
     }
     
     private boolean checkInClusters (int P)
     {
-    	boolean ans=false;
-    	for(int i=0;i<nClust;i++){
-    		ans = clusts[i].contains(P);
-    		if (ans)
-    			return true;
-    	}
-    	return false;
+        boolean ans=false;
+        for(int i=0;i<nClust;i++){
+            ans = clusts[i].contains(P);
+            if (ans)
+                return true;
+        }
+        return false;
     }
-    		
+    
     private List<Integer> regionQuery(int P, double eps)
     {
-    	List<Integer> neighbours = new ArrayList<Integer>();
-    	double tmp;
-    	for(int i=0;i<colors.length;i++){
+        List<Integer> neighbours = new ArrayList<Integer>();
+        double tmp;
+        for(int i=0;i<colors.length;i++){
             if(P!=i){
-            	tmp = dist.compute(colors[P], colors[i]);
-            	if(tmp <= eps)
-            		neighbours.add(i);
+                tmp = dist.compute(colors[P], colors[i]);
+                if(tmp <= eps)
+                    neighbours.add(i);
             }
         }
-    	return neighbours;
+        return neighbours;
     }
 }
